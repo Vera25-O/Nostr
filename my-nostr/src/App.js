b89244ddc3edda5ee73868386902613a138cebe2
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-04-29 22:45:42 +0300

    created footer file

--- my-nostr/src/App.js
+++ my-nostr/src/App.js
@@ -1,11 +1,14 @@
 import LandingPage from "./Components/LandingPage";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <LandingPage/>
+      <Footer/>
+
     </div>
   );
 }
---
+++ my-nostr/src/Components/Footer.js
@@ -0,0 +1,54 @@
+
+
+import { NavLink } from "react-router-dom";
+function Footer() {
+  return (
+    
+
+    
+      <div className="footer">
+        <div className="nav">
+        <nav >
+
+            <p className="fp">Search</p>
+          
+          
+            <p className="fp">About Us</p>
+          
+          
+            <p className="fp">Terms of Use</p>
+          
+          
+            <p className="fp">Refund policy</p>
+          
+          
+            <p className="fp">Privacy & Security</p>
+          
+          
+          
+        </nav>
+        </div>
+        <div className="location">
+        <h4>LOCATION</h4>
+        <p className="lp">Online Thrift store<br></br>
+        <h4>CONTACT</h4><br></br>
+        Email:<br></br>
+         verawakiiru@gmail.com<br></br>
+         Phone:<br></br>
+         +254748438571<br></br>
+         <h4>OPERATION</h4>
+         Open Days:<br></br>
+         Monday -Saturday(8am-6pm)<br></br>
+         Closed:<br></br>
+         Sunday
+        </p>
+        </div>
+        <hr></hr>
+
+
+       
+      <p className="cright">© 2023 veraonuko. All rights reserved.</p>
+    </div>
+  );
+}
+export default Footer;
