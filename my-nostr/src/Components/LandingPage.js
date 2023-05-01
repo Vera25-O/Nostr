e2eb029396f7d4e8235b02a14a0b2fdbcb91c0f1
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-05-01 15:16:37 +0300

    finished section one

--- my-nostr/src/Components/LandingPage.js
+++ my-nostr/src/Components/LandingPage.js
@@ -1,8 +1,21 @@
+import nostr from "./resources/nostr.jpeg";
+
 function LandingPage(){
     return(
+        <>
 <div className="landing">
-    <h1>holla</h1>
+    <img src={nostr} alt="phtoto" className="sec1"/>
+    <h1 className="sec1text">Realtime<br></br>
+      collaboratin Powered<br></br>
+      by Nostr
+      </h1>
+      <button className="started2">Get Started</button>
+
 </div>
+<div className="all">
+
+</div>
+</>
     )
 }
 export default LandingPage;
