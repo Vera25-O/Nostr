d149a22a941b23a12602390cdf30d9c8b10794ed
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-05-02 12:26:29 +0300

    added section two text

--- my-nostr/src/Components/LandingPage.js
+++ my-nostr/src/Components/LandingPage.js
@@ -17,6 +17,13 @@ function LandingPage(){
 <div className="all">
     <p className="use">Used by over 90% of the Precious 100</p>
 <hr></hr>
+<h1 className="sec2text">
+The doc that brings it<br></br>
+all together.
+      </h1>
+      <p className="section2p">Unlike niche workflow tools, Nostr comes with a set of<br></br>
+         powerful building blocks, so you can create the tools your<br></br>
+          team needs.</p>
 </div>
 </>
     )
