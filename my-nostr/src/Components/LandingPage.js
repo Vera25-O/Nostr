be403c13e79a6d7afa3dd79cdb014e9b3dc0b807
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-05-02 12:11:13 +0300

    added hr

--- my-nostr/src/Components/LandingPage.js
+++ my-nostr/src/Components/LandingPage.js
@@ -4,6 +4,7 @@ function LandingPage(){
     return(
         <>
 <div className="landing">
+    {/* section1 */}
     <img src={nostr} alt="phtoto" className="sec1"/>
     <h1 className="sec1text">Realtime<br></br>
       collaboratin Powered<br></br>
@@ -12,8 +13,10 @@ function LandingPage(){
       <button className="started2">Get Started</button>
 
 </div>
+{/* section2 */}
 <div className="all">
-
+    <p className="use">Used by over 90% of the Precious 100</p>
+<hr></hr>
 </div>
 </>
     )
